e8bfee3b9bfc1e1d69087cd3ee3ea699bf32192c
forestnishi <katayamaakihiko@m2.gyao.ne.jp>
2017-03-05 15:47:51 +0900

    create new
    first version

---
+++ squtl.php
@@ -0,0 +1,24 @@
+<?php 
+// get 1
+$T = trim(fgets(STDIN));
+$T = explode(" ", $T);
+// get2
+$md = $T[0];
+$hm = $T[1];
+// get3
+$md = explode("/", $md);
+$mdm = $md[0];
+$mdd = $md[1];
+$hm  = explode(":", $hm);
+$hmh = $hm[0];
+$hmm = $hm[1];
+// up
+$mdd = intval($mdd) + floor($hmh / 24);
+// cut
+$hmh = floor( $hmh % 24 );
+// suppress
+if($mdd < 10) $mdd = "0".$mdd;
+if($hmh < 10) $hmh = "0".$hmm;
+// out
+echo $mdm."/".$mdd." ".$hmh.":".$hmm."\n";
+?>
